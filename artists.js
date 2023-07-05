17b1d70cacd5a4feb4b7cac0b8010e55fcae1367
goeunjib <goeunji0948@naver.com>
2023-07-05 23:38:37 +0900

    article3 grid test

---
+++ artists.js
@@ -0,0 +1,12 @@
+$('.grid').masonry({
+    itemSelector: '.griditem',
+    // columnWidth: 200,
+    gutter: 10,
+    horizontalOrder: true,
+    originLeft: false, //왼쪽부터 정렬 
+});
+
+/* 
+https://gahyun-web-diary.tistory.com/34
+https://wit.nts-corp.com/2022/10/26/6595
+*/
