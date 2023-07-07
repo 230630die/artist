42590a7048faa527dd25cebccf601bc9071d50c9
goeunjib <goeunji0948@naver.com>
2023-07-07 11:26:49 +0900

    0707 완

--- artists.js
+++ artists.js
@@ -1,11 +1,24 @@
-$('.grid').masonry({
-    itemSelector: '.griditem',
-    // columnWidth: 200,
-    gutter: 10,
-    horizontalOrder: true,
-    originLeft: false, //왼쪽부터 정렬 
+function Parallax() {
+  scrollPos = $(this).scrollTop();
+  $("main").css({
+    "background-position": "80% " + -scrollPos / 4 + "px",
+  });
+  $("main h3").css({
+    "margin-top": scrollPos / 4 + "px",
+    opacity: 1 - scrollPos / 250,
+  });
+}
+$(window).scroll(function () {
+  Parallax();
 });
 
+$(window).on("scroll", () => {
+  if (scrollY > 1) {
+    $("main h3").css({ bottom: 0 });
+  } else {
+    $("main h3").css({ bottom: "6.375rem" });
+  }
+});
 /* 
 https://gahyun-web-diary.tistory.com/34
 https://wit.nts-corp.com/2022/10/26/6595
